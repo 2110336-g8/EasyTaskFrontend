cfd97157c44f84825f94254b69cee98228aff4cb
HE KYPTO <zoonyanapat@gmail.com>
2024-02-02 14:23:38 +0700

    Update navBar.tsx

--- my-app/src/components/navbar/navBar.tsx
+++ my-app/src/components/navbar/navBar.tsx
@@ -1,23 +1,28 @@
+"use client";
+
+import * as React from "react";
 import Link from "next/link";
 
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
 export default function NavBar() {
-
-    return (
-        //Just Mock up, feel free to edit
-        <div className="menucontainer fixed px-9 top-0 left-0 right-0 h-20 bg-slate-400 z-30 flex justify-between">
-
-            <div className="flex justify-start items-center z-30 flex-row h-full">
-                <Link className="block px-4 py-2 p text-black hover:font-medium transition duration-00 ease-in-out" href="">
-                    Link1
-                </Link>
-                <Link className="block px-4 py-2 p text-black hover:font-medium transition duration-00 ease-in-out" href="">
-                    Link2
-                </Link>
-                <Link className="block px-4 py-2 p text-black hover:font-medium transition duration-00 ease-in-out" href="">
-                    Link3
-                </Link>
-            </div>
-
-        </div>
-    );
+  return (
+    <div
+      className="py-4 px-6 flex items-center justify-between"
+      style={{ backgroundColor: "#5851dd" }}
+    >
+      <div className="flex items-center">
+        <p className="text-lg font-medium text-slate-200">Easy Task</p>
+      </div>
+    </div>
+  );
 }
