3b6280487a1ba583e14e24b34dbbf561abe2134d
MiGene <gotzeusmlb@gmail.com>
2024-02-05 18:28:50 +0700

    otp api

---
+++ my-app/src/lib/OTPVerification.tsx
@@ -0,0 +1,27 @@
+import { instance } from '@/utils/axiosInstance';
+import { OtpResponse } from '@/types/otp';
+
+export async function otpVerification(
+    email: string, otp: string
+    ): Promise<OtpResponse> {
+    return instance.post("/v1/auth/sendOtp", { email, otp })
+        .then((res) => {
+            const result: OtpResponse = res.data;
+            if (result.success) {
+                // setToken(result.token);
+                console.log(result.message)
+            }
+            return result;
+        })
+        .catch((error) => {
+            if (error.response && error.response.status === 400){
+                console.log(error.response.data)
+                return error.response.data;
+            }
+
+            // console.log(email)
+            console.log(error)
+            return Promise.reject('Can not fetch user');
+        });
+}
+
---
+++ my-app/src/types/otp.ts
@@ -0,0 +1,6 @@
+export interface OtpResponse {
+    error?: string;
+    success: boolean;
+    message: string;
+    email: string;
+}
