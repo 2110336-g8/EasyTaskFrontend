b89f32812319ed25b026817a09b7dfb38a9d58d5
MiGene <gotzeusmlb@gmail.com>
2024-02-07 17:22:46 +0700

    delete some pages

--- my-app/src/app/(auth)/signup/page.tsx
+++ my-app/src/app/(auth)/signup/page.tsx
@@ -4,6 +4,8 @@ import SignupForm from '@/components/signup/signupForm';
 import VerificationForm from '@/components/signup/verificationForm';
 import SignupInfoProvider from '@/context/signupInfoContext';
 import { useEffect, useState } from 'react';
+import PasswordForm from '@/components/signup/passwordForm';
+import BankAccountForm from '@/components/signup/bankAccountForm';
 
 const AuthPage:React.FC = () => {
   const router = useRouter();
@@ -17,9 +19,15 @@ const AuthPage:React.FC = () => {
       <div>
         {authType === 'email' && <SignupForm setAuthType={setAuthType} />}
         {authType === 'verification' && <VerificationForm />}
+        {authType === 'password' && <PasswordForm />}
+        {authType === 'bankAccount' && <BankAccountForm />}
+        
       </div>
     </SignupInfoProvider>
   );
 };
 
 export default AuthPage;
+
+
+
