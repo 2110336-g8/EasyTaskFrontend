b0cfb5183a0b968a208760bcf5fd2f24bad48a21
HE KYPTO <zoonyanapat@gmail.com>
2024-02-09 19:12:10 +0700

    fix datatype of viewTask
    korb kun ti fixed footer krub!

--- my-app/src/components/task/viewTask.tsx
+++ my-app/src/components/task/viewTask.tsx
@@ -116,7 +116,7 @@ const sampleTask: Task = {
     title: 'Design task carddddddddddddddd',
     description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    duration: new Date(), // You can initialize with an actual date
+    duration: new Date(),
     location: 'Some location',
     category: 'Graphics',
     teamSize: 2,
