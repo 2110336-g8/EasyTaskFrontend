29081d6a7e7ac263b278a11bd8e70fdcf62f5925
MiGene <gotzeusmlb@gmail.com>
2024-02-10 18:37:30 +0700

    banner homepage

--- my-app/src/app/(main)/page.tsx
+++ my-app/src/app/(main)/page.tsx
@@ -1,9 +1,11 @@
 'use client';
+import LayoutBox from "@/components/homepage/banner";
 
 export default function Home() {
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            Home Page
+            
+            <LayoutBox />
         </main>
     );
 }
