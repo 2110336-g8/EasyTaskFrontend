34aee775b51cbdff8404b4f651afd038d582bdf1
saimkrua <k.satapornimp@gmail.com>
2024-02-11 02:54:08 +0700

    [EDIT] font footer

--- my-app/src/components/homepage/footer.tsx
+++ my-app/src/components/homepage/footer.tsx
@@ -3,8 +3,8 @@
 export default function Footer() {
     return (
         <div className='bottom-0 py-auto px-auto h-32 w-full flex flex-col items-center justify-center bg-primary-100 text-slate-500 rounded-xl'>
-            <h4 className="font-h4">© 2023 Mayu tomyum Group 8 Software Engineering II</h4>
-            <h4 className="font-h4">Computer Engineering Chulalongkorn University</h4>
+            <div className="font-h4 text-h4 tracking-h4">© 2023 Mayu tomyum Group 8 Software Engineering II</div>
+            <div className="font-h4 text-h4 tracking-h4">Computer Engineering Chulalongkorn University</div>
         </div>
     );
 }
