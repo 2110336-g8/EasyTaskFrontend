1e8888a80472c26827cb17d3bfab1510345a67e7
Punnnnn <punt1374@gmail.com>
2024-02-11 22:19:59 +0700

    tailwind config content edited

--- my-app/tailwind.config.ts
+++ my-app/tailwind.config.ts
@@ -3,10 +3,10 @@ import type { Config } from 'tailwindcss';
 const config = {
     darkMode: ['class'],
     content: [
-        './pages/**/*.{ts,tsx}',
-        './components/**/*.{ts,tsx}',
-        './app/**/*.{ts,tsx}',
+        './my-app/**/*.{ts,tsx}',
         './src/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './pages/**/*.{ts,tsx}',
     ],
     prefix: '',
     theme: {
