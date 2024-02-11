cd0bb293845b33aff2f2be7acb84208dacee0d34
HE KYPTO <zoonyanapat@gmail.com>
2024-02-11 22:01:39 +0700

    Update navBar.tsx

--- my-app/src/components/navbar/navBar.tsx
+++ my-app/src/components/navbar/navBar.tsx
@@ -5,22 +5,20 @@ import {
     PersonIcon,
 } from '@radix-ui/react-icons';
 
-import { Button } from '@/components/ui/button';
-
 export default function NavBar() {
     return (
-        <div className='fixed left-0 top-0 right0 bg-primary-500 py-6 w-full z-50 '>
-            <div className='max-w-7xl mx-16 my-auto px-4 flex items-center justify-between'>
-                <button className='flex flex-col justify-center items-center w-24 h-16 p-2 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
+        <div className='fixed left-0 top-0 right-0 bg-primary-500 py-6 w-full z-50'>
+            <div className='max-w-7xl mx-20 my-auto flex items-center justify-between'>
+                <button className='flex flex-col justify-center items-center w-24 h-16 pl-200 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                     <a
                         href='/'
-                        className='text-white hover:text-gray-300 text-lg font-semibold ml-4'
+                        className='text-white hover:text-gray-300 text-lg font-semibold'
                     >
                         EasyTask
                     </a>
                 </button>
-                <ul className='flex items-center justify-end'>
-                    <li className='flex items-center'>
+                <ul className='flex items-center justify-end space-x-4 h-[64px] ml-auto'> {/* Added ml-auto class here */}
+                    <li>
                         <button className='flex flex-col justify-center items-center w-24 h-16 p-2 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                             <EnvelopeClosedIcon className='h-5 w-5' />
                             <a href='#' className='text-sm'>
@@ -28,7 +26,7 @@ export default function NavBar() {
                             </a>
                         </button>
                     </li>
-                    <li className='flex items-center'>
+                    <li>
                         <button className='flex flex-col justify-center items-center w-24 h-16 p-2 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                             <BackpackIcon className='h-5 w-5' />
                             <a href='#' className='text-sm'>
@@ -36,7 +34,7 @@ export default function NavBar() {
                             </a>
                         </button>
                     </li>
-                    <li className='flex items-center'>
+                    <li>
                         <button className='flex flex-col justify-center items-center w-24 h-16 p-2 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                             <Pencil2Icon className='h-5 w-5' />
                             <a href='#' className='text-sm'>
@@ -44,7 +42,7 @@ export default function NavBar() {
                             </a>
                         </button>
                     </li>
-                    <li className='flex items-center'>
+                    <li>
                         <button className='flex flex-col justify-center items-center w-24 h-16 p-2 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                             <PersonIcon className='h-5 w-5' />
                             <a href='#' className='text-sm'>
