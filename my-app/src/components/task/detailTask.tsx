115e79cd7dfb64ed7cb05fb6de352f26d0d0187c
saimkrua <k.satapornimp@gmail.com>
2024-02-14 02:50:56 +0700

    [EDIT] task detail UI

--- my-app/src/components/task/detailTask.tsx
+++ my-app/src/components/task/detailTask.tsx
@@ -17,7 +17,7 @@ export default function ViewTask(props: ViewTaskProps) {
                     <h1 className='max-w-[500px] text-slate-900 text-balance break-words '>
                         {props.title}
                     </h1>
-                    <Button className=''>Apply Now</Button>
+                    <Button>Apply Now</Button>
                 </div>
                 <div className='w-full h-[360px] '>
                     <img
@@ -27,44 +27,48 @@ export default function ViewTask(props: ViewTaskProps) {
                     />
                 </div>
                 <div className='flex flex-col gap-[24px]'>
-                    <div className='flex justify-between items-center flex-grow flex-shrink'>
-                        <div className='flex flex-col gap-[8px] items-start flex-grow flex-shrink'>
-                            <h4 className='text-slate-900'>Category</h4>
-                            <p className='px-[12px] py-[4px] rounded-[6px] border-[1px] border-primary-500 text-primary-500'>
-                                {props.category}
+                    <div className='grid grid-cols-3 grid-rows-2 items-center gap-y-[8px]'>
+                        <h4 className='text-slate-900 justify-self-start'>
+                            Category
+                        </h4>
+                        <h4 className='text-slate-900 justify-self-center'>
+                            Team
+                        </h4>
+                        <h4 className='text-slate-900 justify-self-end'>
+                            All Wages
+                        </h4>
+                        <p className='justify-self-start px-[12px] py-[4px] rounded-[6px] border-[1px] border-primary-500 text-primary-500'>
+                            {props.category}
+                        </p>
+                        <h4 className='justify-self-center text-center text-slate-700'>
+                            {props.workers}
+                        </h4>
+                        <h2 className='justify-self-end text-slate-700'>
+                            ฿ {props.wages}
+                        </h2>
+                    </div>
+                    {props.description ? (
+                        <div className='flex flex-col gap-[8px]'>
+                            <h4 className='text-slate-900'>Description</h4>
+                            <p className='text-slate-600 text-pretty break-words'>
+                                {props.description}
                             </p>
                         </div>
-                        <div className='flex flex-col gap-[8px] items-center flex-grow flex-shrink'>
-                            <h4 className='text-slate-900'>Team</h4>
-                            <div className='h-10 flex flex-col justify-end'>
-                                <h4 className='text-center text-slate-700'>
-                                    {props.workers}
-                                </h4>
-                            </div>
-                        </div>
-                        <div className='flex flex-col gap-[8px] items-end flex-grow flex-shrink'>
-                            <h4 className='text-slate-900'>All Wages</h4>
-                            <h2 className='text-slate-700'>฿ {props.wages}</h2>
-                        </div>
-                    </div>
-                    <div className='flex flex-col gap-[8px]'>
-                        <h4 className='text-slate-900'>Description</h4>
-                        <p className='text-slate-600 text-pretty break-words'>
-                            {props.description}
-                        </p>
-                    </div>
+                    ) : null}
                     <div className='flex flex-col gap-[8px]'>
                         <h4 className='text-slate-900'>Duration</h4>
                         <p className='text-slate-600'>
                             {props.startDate} - {props.endDate}
                         </p>
                     </div>
-                    <div className='flex flex-col gap-[8px]'>
-                        <h4 className='text-slate-900'>Location</h4>
-                        <p className='text-slate-600'>{props.location}</p>
-                        {/* change to map later */}
-                    </div>
                 </div>
+                    {props.location ? (
+                        <div className='flex flex-col gap-[8px]'>
+                            <h4 className='text-slate-900'>Location</h4>
+                            <p className='text-slate-600'>{props.location}</p>
+                            {/* change to map later */}
+                        </div>
+                    ) : null}
                 <Button className='w-full'>Apply Now</Button>
             </div>
         </div>
