1c2800815fbe961e19a38397c4a39e25d6ca25b8
saimkrua <k.satapornimp@gmail.com>
2024-02-14 21:37:09 +0700

    [FIX] align navbar

--- my-app/src/components/homepage/navbar/navBar.tsx
+++ my-app/src/components/homepage/navbar/navBar.tsx
@@ -7,8 +7,8 @@ import {
 
 export default function NavBar() {
     return (
-        <div className='fixed left-0 top-0 right-0 bg-primary-500 py-6 w-full z-50'>
-            <div className='max-w-7xl mx-20 my-auto flex items-center justify-between'>
+        <div className='fixed left-0 top-0 right-0 bg-primary-500 w-full h-[120px] z-50 flex items-center justify-center'>
+            <div className='h-full w-full desktop:w-[1200px] desktop-l:w-[1328px] flex items-center justify-between'>
                 <button className='flex flex-col justify-center items-center w-24 h-16 pl-200 gap-1 text-white font-medium hover:text-gray-300 rounded-md'>
                     <a
                         href='/'
