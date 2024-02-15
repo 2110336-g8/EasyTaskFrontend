aac7c1997c8fd99a0c5985f16e65b8dcc2fab735
saimkrua <k.satapornimp@gmail.com>
2024-02-15 10:32:09 +0700

    [EDIT] fix flash peek

--- my-app/src/components/ProtectedRoute.tsx
+++ my-app/src/components/ProtectedRoute.tsx
@@ -1,21 +1,24 @@
 'use client';
-import { StorageKeys, clientStorage } from '@/utils/storageService';
+import { clientStorage } from '@/utils/storageService';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({
     children,
 }) => {
     const router = useRouter();
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
         const token: string | null = clientStorage.get().token;
         console.log('token', token);
         if (!token) {
             router.push('/login');
+            return;
         }
+        setLoading(false);
     }, []);
-    return children;
+    return isLoading ? null : children;
 };
 
 export default ProtectedRoute;
