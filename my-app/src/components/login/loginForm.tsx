ccfbbeb0cc016095dcf560c8b728f313e05152ef
MiGene <gotzeusmlb@gmail.com>
2024-02-19 14:51:09 +0700

    login edit

--- my-app/src/components/login/loginForm.tsx
+++ my-app/src/components/login/loginForm.tsx
@@ -72,82 +72,98 @@ export default function LoginForm() {
     };
 
     return (
-        <div className='flex items-center justify-center h-screen font-sans'>
-            <Card className='w-[640px] p-10'>
-                <CardHeader className='text-center'>
-                    <CardTitle className='font-bold text-[40px] tracking-[-0.01em]'>Login</CardTitle>
-                </CardHeader>
-                <Form {...form} >
-                    <form onSubmit={form.handleSubmit(onSubmit)}>
-                        <CardContent>
-                            <div className='grid w-full items-center gap-4'>
-                                <div className='flex flex-col space-y-1.5'>
-                                    <FormField
-                                        control={form.control}
-                                        name='email'
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormLabel className='text-black font-p text-p tracking-p'>
-                                                    Email
-                                                </FormLabel>
-                                                <FormControl>
-                                                    <Input
-                                                        placeholder='user.easytask@email.com'
-                                                        className='font-small text-p tracking-small'
-                                                        {...field}
-                                                    />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
-                                </div>
-                                <div className='flex flex-col space-y-1.5'>
-                                    <FormField
-                                        control={form.control}
-                                        name='password'
-                                        render={({ field }) => (
-                                            <FormItem>
-                                                <FormLabel className='text-black font-p text-p tracking-p'>
-                                                    Password
-                                                </FormLabel>
-                                                <FormControl>
-                                                    <Input
-                                                        className='font-small text-p tracking-small'
-                                                        placeholder='Enter your password'
-                                                        type='password'
-                                                        {...field}
-                                                    />
-                                                </FormControl>
-                                                <FormMessage />
-                                            </FormItem>
-                                        )}
-                                    />
-                                </div>
-                            </div>
-                        </CardContent>
-                        <CardFooter className='grid w-full items-center gap-3'>
-                            {errors.invalidText ? (
-                                <FormMessage className='text-error-500 text-[16px]'>{`${errors.invalidText.message}`}</FormMessage>
-                            ) : (
-                                <FormMessage>
-                                    <br></br>
-                                </FormMessage>
-                            )}
-                            <Button className='w-full bg-primary-900 text-p font-extra-bold tracking-p text-white'>Login</Button>
-                            <CardDescription className='text-slate-500 text-[16px]'>
-                                Doesn't have any account?{' '}
-                                <a
-                                    href='/signup'
-                                    className='text-primary-700 font-bold hover:text-originalColor'
-                                >
-                                    Sign up
-                                </a>
-                            </CardDescription>
-                        </CardFooter>
-                    </form>
-                </Form>
-            </Card>
+        <div className='flex'>
+            <div className='left-side w-1/2 h-full'>
+                <img
+                        src='/signupPic.jpg'
+                        alt='Image'
+                        className='image object-cover h-full w-full'
+                />
+            </div>
+            <div className='right-side w-1/2 h-full'>
+                <div className='flex flex-col items-center justify-center h-screen font-sans'>
+                    <img
+                            src='/logoEasyTask.jpg'
+                            alt='Image'
+                            className='image object-cover w-[303px] m-[20px]'
+                    />
+                    <Card className='w-[640px] h-[600px] p-10 m-[20px]'>
+                        <CardHeader className='text-center m-[20px]'>
+                            <CardTitle className='font-bold text-[40px] tracking-[-0.01em]'>Login</CardTitle>
+                        </CardHeader>
+                        <Form {...form} >
+                            <form onSubmit={form.handleSubmit(onSubmit)}>
+                                <CardContent>
+                                    <div className='grid w-full items-center gap-4 m-[20px]'>
+                                        <div className='flex flex-col space-y-1.5'>
+                                            <FormField
+                                                control={form.control}
+                                                name='email'
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel className='text-black font-p text-p tracking-p'>
+                                                            Email
+                                                        </FormLabel>
+                                                        <FormControl>
+                                                            <Input
+                                                                placeholder='user.easytask@email.com'
+                                                                className='font-small text-p tracking-small'
+                                                                {...field}
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+                                        </div>
+                                        <div className='flex flex-col space-y-1.5'>
+                                            <FormField
+                                                control={form.control}
+                                                name='password'
+                                                render={({ field }) => (
+                                                    <FormItem>
+                                                        <FormLabel className='text-black font-p text-p tracking-p'>
+                                                            Password
+                                                        </FormLabel>
+                                                        <FormControl>
+                                                            <Input
+                                                                className='font-small text-p tracking-small'
+                                                                placeholder='Enter your password'
+                                                                type='password'
+                                                                {...field}
+                                                            />
+                                                        </FormControl>
+                                                        <FormMessage />
+                                                    </FormItem>
+                                                )}
+                                            />
+                                        </div>
+                                    </div>
+                                </CardContent>
+                                <CardFooter className='grid w-full items-center gap-3 m-[20px]'>
+                                    {errors.invalidText ? (
+                                        <FormMessage className='text-error-500 text-[16px]'>{`${errors.invalidText.message}`}</FormMessage>
+                                    ) : (
+                                        <FormMessage>
+                                            <br></br>
+                                        </FormMessage>
+                                    )}
+                                    <Button className='w-full bg-primary-500 text-p font-extra-bold tracking-p text-white'>Login</Button>
+                                    <CardDescription className='text-slate-500 text-[16px]'>
+                                        Doesn't have any account?{' '}
+                                        <a
+                                            href='/signup'
+                                            className='text-primary-700 font-bold hover:text-originalColor'
+                                        >
+                                            Sign up
+                                        </a>
+                                    </CardDescription>
+                                </CardFooter>
+                            </form>
+                        </Form>
+                    </Card>
+                </div>
+            </div>
         </div>
     );
 }
