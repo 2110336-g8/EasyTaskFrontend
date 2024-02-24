b0f96a4cd882d7bb91e504d2b36a9983433e9d50
papongun <papon.gun2@gmail.com>
2024-02-25 01:38:42 +0700

    profile change section gap

--- my-app/src/app/(main)/account/profile/page.tsx
+++ my-app/src/app/(main)/account/profile/page.tsx
@@ -1,8 +1,8 @@
 import PersonalInfo from "@/components/account/profile/personalInfo";
 
-export default function Settings() {
+export default function Profile() {
     return (
-        <div className="w-full flex flex-col gap-[16px]">
+        <div className="w-full flex flex-col gap-[24px]">
             <h2>Your Profile</h2>
             <div className="desktop:w-[640px] w-full">
                 <PersonalInfo></PersonalInfo>
