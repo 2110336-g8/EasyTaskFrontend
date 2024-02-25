36b9c5330f9955db4a99c58d28e08ddd6f5f3a95
papongun <papon.gun2@gmail.com>
2024-02-25 16:56:37 +0700

    footer to btm

--- my-app/src/components/homepage/footer.tsx
+++ my-app/src/components/homepage/footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     return (
-        <div className='bottom-0 py-[20px] px-auto h-auto w-full flex flex-col items-center justify-center bg-slate-100 text-slate-500 rounded-xl'>
+        <div className='sticky top-[100vh] bottom-0 py-[20px] px-auto h-auto w-full flex flex-col items-center justify-center bg-slate-100 text-slate-500 rounded-xl'>
             <p className="">© 2023 Mayu tomyum Group 8 Software Engineering II</p>
             <p className="">Computer Engineering Chulalongkorn University</p>
         </div>
