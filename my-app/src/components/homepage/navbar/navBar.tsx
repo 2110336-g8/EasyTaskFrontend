96374b17aa421a13cd9b7f9c67d4d2927ee598dd
HE KYPTO <zoonyanapat@gmail.com>
2024-03-01 08:58:23 +0700

    [EDIT] Navbar component and add logout handler
    !!Logout component not tested

--- my-app/src/components/homepage/navbar/navBar.tsx
+++ my-app/src/components/homepage/navbar/navBar.tsx
@@ -1,25 +1,25 @@
+"use client"
+
 import {
     EnvelopeClosedIcon,
     BackpackIcon,
     Pencil2Icon,
     PersonIcon,
 } from '@radix-ui/react-icons';
+
 import { clientStorage } from '@/utils/storageService';
 import LoginNavbar from './LoginNavbar';
 import LogoutNavbar from './LogoutNavbar';
 
+type LogoutHandler = () => void;
+
 export default function NavBar() {
     const isLoggedIn = !!clientStorage.get().token;
-
-    const handleLogout = () => {
-        clientStorage.remove();
-        window.location.reload();
-    };
 
     return (
         <div>
             {!isLoggedIn ? (
-                <LoginNavbar handleLogout={handleLogout} />
+                <LoginNavbar />
             ) : (
                 <LogoutNavbar />
             )}
