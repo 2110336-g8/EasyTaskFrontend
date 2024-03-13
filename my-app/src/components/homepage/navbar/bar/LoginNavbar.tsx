72e4ebc6c1794b0115d80c6b172feed798e2ae34
не круто <tsunyanapat@gmail.com>
2024-03-14 03:07:29 +0700

    [FIX] navbar

--- my-app/src/components/homepage/navbar/bar/LoginNavbar.tsx
+++ my-app/src/components/homepage/navbar/bar/LoginNavbar.tsx
@@ -11,50 +11,49 @@ export default function LoginNavbar() {
     };
 
     return (
-        <div key="loginNavbar" className='fixed left-0 top-0 right-0 bg-slate-100 w-full h-[72px] z-50 flex items-center justify-center gap-8'>
-            <div className='p-2 w-full desktop:w-[1200px] desktop-l:w-[1328px] flex items-center justify-between'>
+        <div key="loginNavbar" className='fixed bg-slate-100 w-full h-[72px] z-50 flex items-center justify-center text-base gap-8 whitespace-nowrap max-md:px-5'>
+            <div className='flex gap-5 justify-between w-full max-w-[1328px] max-md:flex-wrap max-md:max-w-full'>
                 <div className="flex items-center gap-4">
-                    <a href="/" className="relative flex-shrink-0">
-                        <Image src="/logo.svg" alt="Logo" width={58} height={32} className="brightness-normal transition duration-300" />
-                        <div className="absolute inset-0 bg-white opacity-0 hover:opacity-25 transition duration-250"></div>
+                    <a href="/" className="flex gap-5 justify-between max-md:flex-wrap">
+                        <Image src="/logo.svg" alt="Logo" width={58} height={32} loading='lazy' className="shrink-0 my-auto aspect-[1.82] w-[58px]"/>
+                        <div className="absolute inset-0 bg-white opacity-0 transition duration-250"></div>
                     </a>
                     <ul className='flex items-center space-x-4'>
-                        <li className="inline-flex items-end gap-4">
+                        <li className="flex flex-auto gap-4">
                             <a className="flex items-end gap-1" href="/">
-                                <Image src="/navicons/briefcase.svg" alt="job icon" width={32} height={32} className="brightness-normal transition duration-300"/>
+                                <Image src="/navicons/briefcase.svg" alt="job icon" width={32} height={32} loading='lazy' className="shrink-0 w-6 aspect-square "/>
                                 <span className="text-primary-500 font-inter font-semibold text-base leading-6 tracking-tighter">Your Jobs</span>
                             </a>
                         </li>
-                        <li className="inline-flex items-end gap-4">
+                        <li className="flex flex-auto gap-4">
                             <a className="flex items-end gap-1" href="/ads">
-                                <Image src="/navicons/megaphone.svg" alt="ads icon" width={32} height={32} className="brightness-normal transition duration-300"/>
+                                <Image src="/navicons/megaphone.svg" alt="ads icon" width={32} height={32} loading='lazy' className="shrink-0 w-6 aspect-square"/>
                                 <span className="text-primary-500 font-inter font-semibold text-base leading-6 tracking-tighter">Your Ads</span>
                             </a>
                         </li>
-                        <li className="inline-flex items-end gap-4">
+                        <li className="flex flex-auto gap-4">
                             <a className="flex items-end gap-1" href="/inbox">
-                                <Image src="/navicons/send.svg" alt="inbox icon" width={32} height={32} className="brightness-normal transition duration-300"/>
+                                <Image src="/navicons/send.svg" alt="inbox icon" width={32} height={32} loading='lazy' className="shrink-0 w-6 aspect-square"/>
                                 <span className="text-primary-500 font-inter font-semibold text-base leading-6 tracking-tighter">Inbox</span>
                             </a>
                         </li>
                     </ul>
                 </div>
                 <ul className='flex items-center space-x-4'>
-                    <li className="inline-flex items-end gap-4">
+                    <li className="flex flex-auto gap-4">
                         <a className="flex items-end gap-1" href="/profile">
-                            <Image src="/navicons/frame.svg" alt="account icon" width={32} height={32} className="brightness-normal transition duration-300"/>
+                            <Image src="/navicons/frame.svg" alt="account icon" width={32} height={32} loading='lazy' className="shrink-0 w-6 aspect-square"/>
                             <span className="text-primary-500 font-inter font-semibold text-base leading-6 tracking-tighter">Profile</span>
                         </a>
                     </li>
-                    <li className="inline-flex items-end gap-4">
-                        <button className="flex items-end gap-1 cursor-pointer" onClick={ handleLogout }>
-                            <Image src="/navicons/logout.svg" alt="logout icon" width={32} height={32} className="brightness-normal transition duration-300"/>
+                    <li className="flex flex-auto gap-4">
+                        <a className="flex items-end gap-1" onClick={ handleLogout }>
+                            <Image src="/navicons/logout.svg" alt="logout icon" width={32} height={32} loading='lazy' className="shrink-0 w-6 aspect-square"/>
                             <span className="text-primary-500 font-inter font-semibold text-base leading-6 tracking-tighter">Logout</span>
-                        </button>
+                        </a>
                     </li>
                 </ul>
             </div>
         </div>
     )
 }
-
--- my-app/src/components/homepage/navbar/bar/LogoutNavbar.tsx
+++ my-app/src/components/homepage/navbar/bar/LogoutNavbar.tsx
@@ -7,15 +7,15 @@ export default function LogoutNavbar() {
         <div className='fixed left-0 top-0 right-0 bg-slate-100 w-full h-[72px] z-50 flex items-center justify-center gap-8'>
             <div className='p-400 w-full desktop:w-[1200px] desktop-l:w-[1328px] flex items-center justify-between'>
                 <a href="/" className="relative">
-                    <Image src="/logo.svg" alt="Logo" width={58} height={32} className="brightness-normal transition duration-300" />
+                    <Image src="/logo.svg" alt="Logo" width={58} height={32} loading='lazy' className="brightness-normal transition duration-300" />
                     <div className="absolute inset-0 bg-white opacity-0 hover:opacity-25 transition duration-250"></div>
                 </a>
 
-                <ul className='flex items-center justify-end space-x-4 h-[64px] ml-auto'>
-                    <Button variant="outline" className='border-primary-500 text-primary-500 hover:text-primary-500' asChild>
+                <ul className='flex gap-5 justify-between'>
+                    <Button variant="outline" className='grow justify-center px-5 py-2 border-primary-500 text-primary-500 hover:text-primary-500' asChild>
                         <Link href="/login">Log in</Link>
                     </Button>
-                    <Button variant="outline" className='border-primary-500 bg-primary-500 hover:bg-primary-500 text-white hover:text-slate-300' asChild>
+                    <Button variant="outline" className='grow justify-center px-5 py-2 border-primary-500 bg-primary-500 hover:bg-primary-500 text-white hover:text-slate-300' asChild>
                         <Link href="/signup">Sign up</Link>
                     </Button>
                 </ul>
