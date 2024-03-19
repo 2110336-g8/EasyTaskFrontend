692be609a4754b959130da48e8b159299e57ed6b
MiGene <gotzeusmlb@gmail.com>
2024-03-19 15:32:36 +0700

    job first commit

---
+++ my-app/src/app/(main)/(protected)/job/page.tsx
@@ -0,0 +1,178 @@
+'use client';
+import { useEffect, useState } from 'react';
+import {
+    AdsCardProps,
+    GetUserAdsResponse,
+    TaskStateOptions,
+} from '@/types/task';
+import { Button } from '@/components/ui/button';
+import { PlusIcon, PenSquareIcon, PenBoxIcon } from 'lucide-react';
+import AdsToggleList from '@/components/adsList/adsToggleList';
+import { useRouter } from 'next/navigation';
+import dayjs from 'dayjs';
+import { toast } from '@/components/ui/use-toast';
+import { clientStorage } from '@/utils/storageService';
+import { getUserAds } from '@/lib/getUserAds';
+
+export type WageRange = [number | null, number | null];
+
+// const mockList: AdsCardProps[] = [
+//     {
+//         taskId: 'someTaskId',
+//         title: 'someTitle',
+//         category: 'someCategory',
+//         location: 'someLocation',
+//         wages: '5,000,000',
+//         startDate: 'someStartDate',
+//         applications: '1,000',
+//     },
+//     {
+//         taskId: 'someTaskId',
+//         title: 'someTitle',
+//         category: 'someCategory',
+//         location: 'someLocation',
+//         wages: '1,000',
+//         startDate: 'someStartDate',
+//         applications: '1,000',
+//     },
+//     {
+//         taskId: 'someTaskId',
+//         title: 'someTitle',
+//         category: 'someCategory',
+//         location: 'someLocation',
+//         wages: '1,000',
+//         startDate: 'someStartDate',
+//         applications: '1,000',
+//     },
+//     {
+//         taskId: 'someTaskId',
+//         title: 'someTitle',
+//         category: 'someCategory',
+//         location: 'someLocation',
+//         wages: '1,000',
+//         startDate: 'someStartDate',
+//         applications: '1,000',
+//     },
+// ];
+
+export default function AdsList() {
+    const router = useRouter();
+    const [adsPayList, setAdsPayList] = useState<AdsCardProps[]>([]);
+    const [adsOpenList, setAdsOpenList] = useState<AdsCardProps[]>([]);
+    const [adsWorkList, setAdsWorkList] = useState<AdsCardProps[]>([]);
+    const [adsClosedList, setAdsClosedList] = useState<AdsCardProps[]>([]);
+    const [isManaging, setIsManaging] = useState(false);
+
+    useEffect(() => {
+        const userId: string | null = clientStorage.get().user._id;
+        if (!userId) {
+            router.push('/login');
+        }
+        // console.log(userId);
+        // const userId: string | null = '65eff56288030343046799b0';
+        const fetchAdsList = async () => {
+            getUserAds({
+                userId,
+            })
+                .then((adsListData: GetUserAdsResponse) => {
+                    const formattedAdsList: AdsCardProps[] =
+                        adsListData.tasks.map(task => ({
+                            taskId: task._id,
+                            image: task.image,
+                            title: task.title,
+                            status: task.status,
+                            startDate: dayjs(task.startDate).format(
+                                'DD MMM YYYY',
+                            ),
+                            endDate: dayjs(task.endDate).format('DD MMM YYYY'),
+                            location: task.location?.name,
+                            applications: task.workers.toLocaleString(),
+                            wages: task.wages.toLocaleString(),
+                            category: task.category,
+                        }));
+                    console.log(adsListData);
+
+                    setAdsPayList(
+                        formattedAdsList.filter(
+                            task => task.status == TaskStateOptions.COMPLETED,
+                        ),
+                    );
+                    setAdsOpenList(
+                        formattedAdsList.filter(
+                            task => task.status == TaskStateOptions.OPEN,
+                        ),
+                    );
+                    setAdsWorkList(
+                        formattedAdsList.filter(
+                            task => task.status == TaskStateOptions.INPROGRESS,
+                        ),
+                    );
+                    setAdsClosedList(
+                        formattedAdsList.filter(
+                            task => task.status == TaskStateOptions.CLOSED,
+                        ),
+                    );
+                })
+                .catch(e => {
+                    toast({
+                        variant: 'destructive',
+                        title: 'Uh oh! Something went wrong.',
+                        description: 'There was a problem with your request.',
+                    });
+                    console.error('Error fetching ads list', e);
+                });
+        };
+        fetchAdsList();
+    }, []);
+
+    // Function to set isManaging to true
+    const handleManage = () => {
+        setIsManaging(true);
+    };
+
+    return (
+        <main className='flex flex-col gap-[40px] items-center '>
+            <div className='w-full flex justify-between'>
+                <h1>Your Job</h1>
+                {/* <div className='flex gap-[16px]'>
+                    <Button
+                        onClick={() => {
+                            router.push('/task/create');
+                        }}
+                        className='gap-x-[10px] text-primary-500 bg-slate-50 border border-primary-500 border-[2px] hover:bg-slate-200'
+                    >
+                        <PlusIcon />
+                        Create
+                    </Button>
+                    <Button
+                        onClick={handleManage}
+                        className='gap-x-[10px] text-primary-500 bg-slate-50 border border-primary-500 border-[2px]'
+                    >
+                        <PenSquareIcon />
+                        Manage
+                    </Button>
+                </div> */}
+            </div>
+            <AdsToggleList
+                type='pay'
+                adsList={adsPayList}
+                managing={isManaging}
+            />
+            <AdsToggleList
+                type='open'
+                adsList={adsOpenList}
+                managing={isManaging}
+            />
+            <AdsToggleList
+                type='working'
+                adsList={adsWorkList}
+                managing={isManaging}
+            />
+            <AdsToggleList
+                type='closed'
+                adsList={adsClosedList}
+                managing={isManaging}
+            />
+        </main>
+    );
+}
---
+++ my-app/src/components/jobList/jobToggleList.tsx
@@ -0,0 +1,63 @@
+'use client';
+import { useState } from 'react';
+import { AdsCardProps } from '@/types/task';
+import AdsCard from '@/components/adsList/adsCard';
+import { ChevronDownIcon, ChevronRightIcon } from 'lucide-react';
+
+export default function AdsToggleList({
+    type,
+    adsList,
+    managing,
+}: {
+    type: keyof typeof names;
+    adsList: AdsCardProps[];
+    managing: boolean;
+}) {
+    const names = {
+        pay: 'To Pay Deposit',
+        open: 'Open for Apply',
+        working: 'Working On',
+        closed: 'Closed',
+    };
+
+    const [isShow, setIsShow] = useState<boolean>(false);
+    const [buttonFuncType, setButtonFuncType] = useState<string>(type);
+    if (managing) {
+        setButtonFuncType('managing');
+    }
+
+    return (
+        <div className='w-full flex flex-col gap-[20px]'>
+            <div
+                className='group flex w-full gap-[8px] cursor-pointer'
+                onClick={() => setIsShow(!isShow)}
+            >
+                <button>
+                    {isShow ? (
+                        <ChevronDownIcon className='stroke-slate-500 stroke-2 w-[28px] h-[28px] group-hover:stroke-primary-500' />
+                    ) : (
+                        <ChevronRightIcon className='stroke-slate-500 stroke-2 w-[28px] h-[28px] group-hover:stroke-primary-500' />
+                    )}
+                </button>
+                <h4 className='text-slate-500 group-hover:text-primary-500 group-hover:font-medium'>
+                    {names[type]}
+                </h4>
+            </div>
+            {isShow ? (
+                adsList.length > 0 ? (
+                    <div className='w-fit'>
+                        <div className='flex flex-col gap-[24px] tablet:grid-cols-2 laptop:grid-cols-3 desktop-l:grid-cols-4 w-full gap-y-[24px] justify-between'>
+                            {adsList.map((task, index) => (
+                                <AdsCard
+                                    key={index}
+                                    {...task}
+                                    buttonFunc={type}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                ) : null
+            ) : null}
+        </div>
+    );
+}
