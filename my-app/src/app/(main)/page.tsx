320db84f2853655d8a1b310502075ab12127aadd
papongun <papon.gun2@gmail.com>
2024-03-19 14:04:57 +0700

    loginPushToTask , rm getSelf

--- my-app/src/app/(main)/page.tsx
+++ my-app/src/app/(main)/page.tsx
@@ -1,10 +1,23 @@
 'use client';
-import LayoutBox from "@/components/homepage/banner";
+import LayoutBox from '@/components/homepage/banner';
+import { clientStorage } from '@/utils/storageService';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+    const [isLoading, setLoading] = useState(true);
+    const router = useRouter();
+
+    useEffect(() => {
+        const user = clientStorage.get().user;
+        if (user) {
+            router.push('/task');
+        }
+        setLoading(true);
+    }, []);
+
     return (
         <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-            
             <LayoutBox />
         </main>
     );
