6154ab5eee1375a8d5ffa340241e73047916d509
HE KYPTO <zoonyanapat@gmail.com>
2024-03-23 18:40:06 +0700

    Update profileCard.tsx

--- my-app/src/components/profile/profileCard.tsx
+++ my-app/src/components/profile/profileCard.tsx
@@ -18,7 +18,7 @@ export default function ProfileCard(props: UserCard) {
                             alt="Profile Image"
                             width={200}
                             height={200}
-                            className="rounded-full border-4 border-white border-solid"
+                            className="rounded-full border-4 border-white border-solid object-cover"
                             style={{ backgroundColor: props.imageUrl ? "transparent": "white"}}
                         />
                         </div>
