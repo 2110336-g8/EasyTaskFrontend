a576ada3143b0b739e0bd477dedc6c4fe354fdef
papongun <papon.gun2@gmail.com>
2024-03-25 23:09:14 +0700

    add side padding sm-width

--- my-app/src/app/(main)/(protected)/messages/layout.tsx
+++ my-app/src/app/(main)/(protected)/messages/layout.tsx
@@ -4,7 +4,7 @@ export default async function InboxLayout({
     children: React.ReactNode;
 }) {
     return (
-        <div className='w-full h-fit desktop:w-[1000px] self-center'>
+        <div className='w-full h-fit desktop:w-[1000px] self-center px-[16px] desktop:px-[40px]'>
             {children}
         </div>
     );
