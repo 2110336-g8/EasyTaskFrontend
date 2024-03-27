a4a3d3dff47b18b3ec8fe0c768a527626b5c3b9e
paychorn <Rujjirap@gmail.com>
2024-03-28 04:39:00 +0700

    Update navbar styles

--- my-app/src/components/homepage/navbar/bar/EmptyNavbar.tsx
+++ my-app/src/components/homepage/navbar/bar/EmptyNavbar.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image';
 
 export default function EmptyNavbar() {
     return (
-        <div className="fixed left-0 top-0 right-0 z-50 justify-center items-center px-4 md:px-16 py-4 text-base font-medium tracking-normal leading-6 text-indigo-600 bg-slate-100 max-md:px-5">
-            <div className="flex flex-wrap justify-between w-full max-md:flex-no-wrap">
+        <div className="fixed left-0 top-0 right-0 z-50 justify-center items-center px-[200px] md:px-16 py-4 text-base font-medium tracking-normal leading-6 text-indigo-600 bg-slate-100 max-md:px-5">
+            <div className="flex flex-wrap justify-between w-full px-[200px] max-md:flex-no-wrap">
                 <div className="flex gap-5 items-center">
                     <a href="/">
                         <Image
--- my-app/src/components/homepage/navbar/bar/LoginNavbar.tsx
+++ my-app/src/components/homepage/navbar/bar/LoginNavbar.tsx
@@ -18,7 +18,7 @@ export default function LoginNavbar() {
     };
 
     return (
-        <div className='fixed left-0 top-0 right-0 z-50 justify-center items-center px-4 md:px-16 py-4 text-base font-medium tracking-normal leading-6 text-indigo-600 bg-slate-100 max-md:px-5'>
+        <div className='fixed left-0 top-0 right-0 z-50 justify-center items-center px-[200px] md:px-16 py-4 text-base font-medium tracking-normal leading-6 text-indigo-600 bg-slate-100 max-md:px-5'>
             <div className='flex flex-wrap justify-between w-full max-md:flex-no-wrap'>
                 <div className='flex gap-5'>
                     <a href='/'>
