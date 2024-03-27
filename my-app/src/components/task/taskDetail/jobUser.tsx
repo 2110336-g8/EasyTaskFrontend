3f96c91693e0199831d1df94e938d218fe748f86
HE KYPTO <zoonyanapat@gmail.com>
2024-03-27 10:47:40 +0700

    Update jobUser.tsx

--- my-app/src/components/task/taskDetail/jobUser.tsx
+++ my-app/src/components/task/taskDetail/jobUser.tsx
@@ -8,18 +8,22 @@ export default function JobUser(props: ViewTaskProps) {
     return (
         <div className='flex flex-col gap-[16px]'>
             <h4>About the Client</h4>
-            <Link href={`/profile/${props.customer?._id}`} className='flex flex-row items-center gap-[16px]'>
-                <Avatar key={props.customer?._id} style={{ width: '56px', height: '56px' }}>
-                    <AvatarImage src={props.customer?.image} alt='@shadcn' />
-                    <AvatarFallback>?</AvatarFallback>
-                </Avatar>
-                <div className='flex flex-col gap-[4px]'>
-                    <p>{props.customer?.name}</p>
-                    {props.customer?.phoneNumber ? (
-                        <p>{props.customer.phoneNumber}</p>
-                    ) : null}
-                </div>
-            </Link>
+            <div className='flex flex-row items-center gap-[16px]'>
+                <Link href={`/profile/${props.customer?._id}`}>
+                    <a>
+                    <Avatar key={props.customer?._id} style={{ width: '56px', height: '56px' }}>
+                        <AvatarImage src={props.customer?.image} alt='@shadcn' />
+                        <AvatarFallback>?</AvatarFallback>
+                    </Avatar>
+                    <div className='flex flex-col gap-[4px]'>
+                        <p>{props.customer?.name}</p>
+                        {props.customer?.phoneNumber ? (
+                            <p>{props.customer.phoneNumber}</p>
+                        ) : null}
+                    </div>
+                    </a>
+                </Link>
+            </div>
         </div>
     );
 }
